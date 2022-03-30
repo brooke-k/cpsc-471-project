7c50d0ad473ac8236dc6cb38304cc3028f34e4ac
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-03-30 12:36:25 -0600

    "feat: Added contains vs. does not contain for prod. search? "

--- frontend/src/pages/ProductSearch.jsx
+++ frontend/src/pages/ProductSearch.jsx
@@ -16,6 +16,7 @@ const ProductSearch = () => {
     "Wheat",
     "Triticale",
   ];
+
   return (
     <>
       <h1>Product Search</h1>
@@ -23,11 +24,22 @@ const ProductSearch = () => {
         {topAllergens.map((e, i) => {
           return (
             <div key={i}>
-              <input type="checkbox" key={i} name={topAllergens[i]}></input>
+              <input type="checkbox" key={i} value={topAllergens[i]}></input>
               <label for={topAllergens[i]}>{topAllergens[i]}</label>
             </div>
           );
         })}
+      </div>
+      <div>
+        <input type="radio" name="containsOpt" value="contains"></input>
+        <label for="contains">Contains</label>
+        <input
+          type="radio"
+          name="containsOpt"
+          value="not_contains"
+          checked
+        ></input>
+        <label for="not_contains">Does not contain</label>
       </div>
     </>
   );
