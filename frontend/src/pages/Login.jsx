b8e81dff35c80c168d4ae550094ea828017b3b5c
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-03-30 11:22:46 -0600

    "feat: Added login page, bare bones"

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,6 +4,9 @@ const Login = () => {
   return (
     <div>
       <h1>This is the Login</h1>
+      <input type="text" placeholder="Email"></input>
+      <input type="password" placeholder="Password"></input>
+      <button>Log In</button>
     </div>
   );
 };
