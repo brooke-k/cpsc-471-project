cc9bbe8c50f5d7f89a1bf7d6d66fac87d955e424
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-03-30 11:15:42 -0600

    "feat: Added inputs and submit button for signup page"

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -4,6 +4,11 @@ const SignUp = () => {
   return (
     <div>
       <h1>This is the SignUp</h1>
+      <input type="text" placeholder="Username"></input>
+      <input type="text" placeholder="Email"></input>
+      <input type="password" placeholder="Password"></input>
+      <input type="password" placeholder="Confirm Password"></input>
+      <button>Sign Up</button>
     </div>
   );
 };
