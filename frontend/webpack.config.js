b1f9c19f707be2f4b9d58f9dc2c86c339ded6516
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-03-30 10:18:13 -0600

    fix: Corrected webpack imports

---
+++ frontend/webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: [
+          "babel-loader",
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                '@babel/preset-env', '@babel/preset-react'
+              ]
+            }
+          },
+        ],
+
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+}
