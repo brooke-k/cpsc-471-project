242bea2a63561122c69e1516df312a104589150b
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-04-07 13:37:14 -0600

    "feat: Added an example for post request to mongodb"

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status, Body
 import config, uvicorn, os, model
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+import json
+from bson import json_util
 
 
 testApp = FastAPI()
+
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
+
 
 @testApp.get("/")
 def index():
@@ -17,6 +25,15 @@ async def get_power (powerSource: str):
         raise HTTPException(status_code=404, detail="Power source not found")
 
 
+@testApp.post("/powermail/", response_description="SEND THE POWER", response_model=model.testModel)
+async def send_power (powerSource: model.testModel = Body(...)):
+    powerSource = jsonable_encoder(powerSource)
+    print(powerSource)
+    new_powerSource = config.db["testcollect"].insert_one(powerSource)
+    if (power := config.db["testcollect"].find_one({"_id": new_powerSource.inserted_id})) is not None:
+        return JSONResponse(status_code=status.HTTP_201_CREATED, content = parse_json(power))
+    else:
+        raise HTTPException(status_code=404, detail="Power source not added")
 
 
 if __name__ == "__main__":
--- backend/model.py
+++ backend/model.py
@@ -18,12 +18,27 @@ class PyObjectId(ObjectId):
     def __modify_schema__(cls, field_schema):
         field_schema.update(type="string")
 
+
+# Used for getting
 class testModel(BaseModel):
     _id: Field(default_factory=PyObjectId, alias="_id")
     powerSource: str
 
     class Config:
         allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+
+          "testData": {
+            "powerSource":"beans"
+          }
+        }
+
+class updateTestModel(BaseModel):
+    powerSource: str
+
+    class Config:
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
         schema_extra = {
