d9feaf8faede17782f79bc40d049bc4e5034a697
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-04-17 13:36:10 -0600

    feat: Admins can now delete products

--- frontend/src/pages/ManageProducts.jsx
+++ frontend/src/pages/ManageProducts.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import axiosJSONInst from "../axios";
 import AdminNavBar from "../components/AdminNavBar";
 import DisplayAllProducts from "../components/DisplayAllProducts";
+import "../styles/prodSearch.scss";
 
 const ManageProducts = () => {
   const [actionNotif, setactionNotif] = useState("");
+  const [productId, setProductID] = useState("");
+
+  const handleProductID = (e) => {
+    setProductID(e.target.value);
+    setactionNotif("");
+  };
+
+  function deleteProduct() {
+    if (productId === "") {
+      setactionNotif("Please enter a product ID");
+      return;
+    }
+    const delString = "/product/remove?product_id=" + productId;
+    axiosJSONInst
+      .delete(delString)
+      .then((res) => {
+        console.log(res);
+        setactionNotif("Successfully deleted product");
+      })
+      .catch((err) => {
+        console.log(err);
+        setactionNotif(
+          'Error: Product with ID "' + productId + '" could not be deleted.'
+        );
+      });
+  }
+
   return (
     <>
       <AdminNavBar />
@@ -49,37 +78,49 @@ const ManageProducts = () => {
                 width: "minContent",
               }}
             >
-              Update User <br></br>Information
+              Update Product <br></br>Information
             </h1>
-            <div id="inputRow" style={{ justifyContent: "center" }}></div>
-          </div>
-          <div style={{ margin: "1rem", padding: "0" }}></div>
-          <div
-            style={{ margin: "1rem", padding: "0" }}
-            // hidden={updateOption === "updateEmail"}
-          >
+            <div style={{ margin: "1rem", padding: "0" }}>
+              <div id="inputRow">
+                <label htmlFor="productID">Product ID</label>
+                <input
+                  type="text"
+                  placeholder="ID Number"
+                  name="productID"
+                  value={productId}
+                  onInput={handleProductID}
+                ></input>
+              </div>
+            </div>
             <div
-              id="inputRow"
-              style={{
-                width: "100%",
-                justifyContent: "center",
-                justifyItems: "center",
-              }}
-            ></div>
-            <div
-              // hidden={updateOption === "updateUsername" ? true : false}
               style={{ margin: "1rem", padding: "0" }}
-            ></div>
-            <div
-              id="inputRow"
-              style={{
-                width: "100%",
-                justifyContent: "center",
-                justifyItems: "center",
-              }}
+              // hidden={updateOption === "updateEmail"}
             >
-              <button>Update User Information</button>
-              <h2>{actionNotif}</h2>
+              <div
+                id="inputRow"
+                style={{
+                  width: "100%",
+                  justifyContent: "center",
+                  justifyItems: "center",
+                }}
+              ></div>
+              <div
+                // hidden={updateOption === "updateUsername" ? true : false}
+                style={{ margin: "1rem", padding: "0" }}
+              ></div>
+              <div
+                id="inputRow"
+                style={{
+                  width: "100%",
+                  justifyContent: "center",
+                  justifyItems: "center",
+                }}
+              >
+                <button onClick={deleteProduct}>Delete Product By ID</button>
+              </div>
+              <div id="inputRow">
+                <h3>{actionNotif}</h3>
+              </div>
             </div>
           </div>
         </div>
